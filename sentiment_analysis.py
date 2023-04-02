61a1ec82f2771683a315ec0707eee8216481ec0b
Pronob <proobdu29@gmail.com>
2023-04-02 14:32:41 -0400

    Added author name and make some necessary variable names changed.

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,8 +1,19 @@
+# @Time    : 3/29/2023 5:58 PM
+# @Author  : Pronob Barman
+# @FileName: sentiment_analysis.py
+# @Software: PyCharm
+
+'''
+This script is used to perform sentiment analysis on the data collected from the survey. (Q85)
+Used nltk library to perform sentiment analysis.
+'''
+
+
 import pandas as pd
 from nltk.sentiment import SentimentIntensityAnalyzer
 
 sentences = pd.read_excel('test.xlsx')['Q85'].tolist()
-summary_table = pd.DataFrame(columns=['sr. no', 'sentence', 'aspect', 'sentiment score', 'label'])
+sentiment_table = pd.DataFrame(columns=['sr. no', 'sentence', 'aspect', 'sentiment score', 'label'])
 
 summary_rows = []
 for i, sentence in enumerate(sentences):
@@ -26,7 +37,8 @@ for i, sentence in enumerate(sentences):
             'sentiment score': sentiment_score,
             'label': label
         })
-summary_table = pd.concat([summary_table, pd.DataFrame(summary_rows)])
-summary_table.to_csv('summary_table.csv', index=False)
-
-print(summary_table)
+# concantenate the summary table with the new rows
+sentiment_table = pd.concat([sentiment_table, pd.DataFrame(summary_rows)])
+sentiment_table.to_csv('sentiment_table.csv', index=False)
+# print sentiment table
+print(sentiment_table)
