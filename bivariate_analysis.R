e414cae8f423497081cd61e953aabbd2b20b055e
Pronob <96884634+pronob29@users.noreply.github.com>
2023-05-05 15:04:46 -0400

    Update bivariate_analysis.R

--- bivariate_analysis.R
+++ bivariate_analysis.R
@@ -10,26 +10,30 @@ data <- na.omit(data)
 # create a grid of plots
 grid <- grid.arrange(
   ggplot(data, aes(x=techScore, y=complexScore)) + 
-    geom_point(aes(color=race)) + 
+    geom_point() + 
     geom_smooth(method = "lm", color = "darkgreen") +
     labs(title="Techscore vs. ComplexScore", 
          x="Techscore", y="ComplexScore"),
   
   ggplot(data, aes(x=techScore, y=health_literacy_score)) + 
-    geom_point(aes(color=race)) + 
+    geom_point() + 
     geom_smooth(method = "lm", color = "darkgreen") +
     labs(title="Techscore vs. Health Literacy Score", 
          x="Techscore", y="Health Literacy Score"),
   
   ggplot(data, aes(x=techScore, y=age)) + 
-    geom_point(aes(color=race)) + 
+    geom_point() + 
     geom_smooth(method = "lm", color = "darkgreen") +
     labs(title="Techscore vs. Age", 
          x="Techscore", y="Age"),
   
-  ncol=3, nrow=1, widths=c(5,5,5)
+  ggplot(data, aes(x=techScore, y=race)) + 
+    geom_point() + 
+    labs(title="Techscore vs. Race", 
+         x="Techscore", y="Race"),
+  
+  ncol=2, nrow=2, widths=c(5,5)
 )
 
 # save the plot as PNG file
-ggsave("bivariate_analysis.png", grid, width=15, height=5)
-
+ggsave("bivariate_analysis.png", grid, width=10, height=10)
